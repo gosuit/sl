da38fb363b504cb590a9f0b195a1a100e9c8098f
Nikita <ns_stepanov@inbox.ru>
2025-03-21 13:02:39 +0300

    Revert "fix bugs (source and with)"
    This reverts commit 1c3fdb0daf2e9e84b4f88b365ed42d7cc49b991e.

--- definition.go
+++ definition.go
@@ -2,10 +2,8 @@ package sl
 
 import (
 	"context"
-	"fmt"
 	"log/slog"
 	"os"
-	"runtime"
 )
 
 type logStruct struct {
@@ -22,17 +20,15 @@ func (l *logStruct) Handler() Handler {
 }
 
 func (l *logStruct) With(args ...any) Logger {
-	l2 := *l
-	l2.log = l.log.With(args...)
+	l.log = l.log.With(args...)
 
-	return &l2
+	return l
 }
 
 func (l *logStruct) WithGroup(name string) Logger {
-	l2 := *l
-	l2.log = l.log.WithGroup(name)
+	l.log = l.log.WithGroup(name)
 
-	return &l2
+	return l
 }
 
 func (l *logStruct) ToSlog() *slog.Logger {
@@ -44,8 +40,6 @@ func (l *logStruct) Enabled(ctx context.Context, level Level) bool {
 }
 
 func (l *logStruct) Log(ctx context.Context, level Level, msg string, args ...any) {
-	l = l.addSourceToLogByCfg()
-
 	l.log.Log(ctx, level, msg, args...)
 }
 
@@ -54,54 +48,43 @@ func (l *logStruct) LogAttrs(ctx context.Context, level Level, msg string, attrs
 }
 
 func (l *logStruct) Debug(msg string, args ...any) {
-	l.Log(context.Background(), LevelDebug, msg, args...)
+	l.log.Debug(msg, args...)
 }
 
 func (l *logStruct) DebugContext(ctx context.Context, msg string, args ...any) {
-	l.Log(ctx, LevelDebug, msg, args)
+	l.log.DebugContext(ctx, msg, args...)
 }
 
 func (l *logStruct) Info(msg string, args ...any) {
-	l.Log(context.Background(), LevelInfo, msg, args...)
+	l.log.Info(msg, args...)
 }
 
 func (l *logStruct) InfoContext(ctx context.Context, msg string, args ...any) {
-	l.Log(ctx, LevelInfo, msg, args...)
+	l.log.InfoContext(ctx, msg, args...)
 }
 
 func (l *logStruct) Warn(msg string, args ...any) {
-	l.Log(context.Background(), LevelWarn, msg, args...)
+	l.log.Warn(msg, args...)
 }
 
 func (l *logStruct) WarnContext(ctx context.Context, msg string, args ...any) {
-	l.Log(ctx, LevelWarn, msg, args...)
+	l.log.WarnContext(ctx, msg, args...)
 }
 
 func (l *logStruct) Error(msg string, args ...any) {
-	l.Log(context.Background(), LevelError, msg, args...)
+	l.log.Error(msg, args...)
 }
 
 func (l *logStruct) ErrorContext(ctx context.Context, msg string, args ...any) {
-	l.Log(ctx, LevelError, msg, args...)
+	l.log.ErrorContext(ctx, msg, args...)
 }
 
 func (l *logStruct) Fatal(msg string, args ...any) {
-	l.Log(context.Background(), LevelError, msg, args...)
+	l.log.Error(msg, args...)
 	os.Exit(1)
 }
 
 func (l *logStruct) FatalContext(ctx context.Context, msg string, args ...any) {
-	l.Log(ctx, LevelError, msg, args...)
+	l.log.ErrorContext(ctx, msg, args...)
 	os.Exit(1)
-}
-
-func (l *logStruct) addSourceToLogByCfg() *logStruct {
-	_, file, line, _ := runtime.Caller(3)
-
-	if l.cfg.AddSource {
-		a := slog.String(slog.SourceKey, fmt.Sprintf("%s:%d", file, line))
-		l = l.With(a).(*logStruct)
-	}
-
-	return l
 }
